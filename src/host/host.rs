4df640406e88b3e3dc3157098651788b05fa8ee0
Nik Deapen <nik@deapen.com>
2024-02-04 10:38:37 -0800

    [host] Added hosts & host references.

---
+++ src/host/host.rs
@@ -0,0 +1,53 @@
+use crate::{Domain, IPAddress};
+
+/// Either a domain or an IP address.
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+pub enum Host {
+    /// A domain.
+    Name(Domain),
+
+    /// An IP address.
+    Address(IPAddress),
+}
+
+impl From<Domain> for Host {
+    fn from(domain: Domain) -> Self {
+        Self::Name(domain)
+    }
+}
+
+impl<A: Into<IPAddress>> From<A> for Host {
+    fn from(ip: A) -> Self {
+        Self::Address(ip.into())
+    }
+}
+
+impl Host {
+    //! Matching
+
+    /// Checks if the host is a domain.
+    pub const fn is_domain(&self) -> bool {
+        matches!(self, Self::Name(_))
+    }
+
+    /// Checks if the host is an IP address.
+    pub const fn is_ip(&self) -> bool {
+        matches!(self, Self::Address(_))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Domain, Host, IPv4Address};
+
+    #[test]
+    fn matching() {
+        let host: Host = Domain::localhost().into();
+        assert!(host.is_domain());
+        assert!(!host.is_ip());
+
+        let host: Host = IPv4Address::LOCALHOST.into();
+        assert!(!host.is_domain());
+        assert!(host.is_ip());
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub use domain::*;
 pub use endpoint::*;
+pub use host::*;
 pub use ip::*;
 pub use socket::*;
 
 mod domain;
 mod endpoint;
+mod host;
 mod ip;
 mod socket;
