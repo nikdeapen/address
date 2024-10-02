a460b85b663f4171157e76b9662672afb708f718
Nik Deapen <nik@deapen.com>
2024-10-02 08:46:51 -0700

    [host] Added support for hosts.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 pub use domain::*;
 pub use endpoint::*;
+pub use host::*;
 pub use ip::*;
 pub use parse::*;
 pub use socket::*;
 
 mod domain;
 mod endpoint;
+mod host;
 mod ip;
 mod parse;
 mod socket;
