9152419ca8836439b1167a161ce22b3cdabc1448
Nik Deapen <nik@deapen.com>
2025-02-09 22:52:30 -0800

    [*] Added support for parsing addresses.

--- src/socket/mod.rs
+++ src/socket/mod.rs
@@ -2,9 +2,11 @@ pub use socket_address::*;
 pub use socket_address_v4::*;
 pub use socket_address_v6::*;
 
-mod conversions;
-mod conversions_std;
-mod display;
 mod socket_address;
 mod socket_address_v4;
 mod socket_address_v6;
+
+mod conversions;
+mod conversions_std;
+mod display;
+mod from_str;
