5e64e2e9382f5e350f0b46da4837893b50c273f2
Nik Deapen <nik@deapen.com>
2026-08-14 13:10:13 -0700

    [socket] Added direct authority conversions,

--- src/socket/conversions_v4.rs
+++ src/socket/conversions_v4.rs
@@ -1,4 +1,4 @@
-use crate::{SocketAddress, SocketAddressV4};
+use crate::{Authority, SocketAddress, SocketAddressV4};
 
 impl SocketAddressV4 {
     //! Conversions
@@ -7,17 +7,30 @@ impl SocketAddressV4 {
     pub const fn to_socket(self) -> SocketAddress {
         SocketAddress::new(self.ip().to_ip(), self.port())
     }
+
+    /// Converts the IPv4 socket address to an authority.
+    pub const fn to_authority(self) -> Authority {
+        Authority::new(self.ip().to_host(), self.port())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{IPv4Address, SocketAddress, SocketAddressV4};
+    use crate::{Authority, IPv4Address, SocketAddress, SocketAddressV4};
 
     #[test]
     fn v4_to_socket() {
         let socket: SocketAddressV4 = IPv4Address::LOCALHOST.to_socket(80);
         let result: SocketAddress = socket.to_socket();
         let expected: SocketAddress = SocketAddress::new(IPv4Address::LOCALHOST.to_ip(), 80);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn v4_to_authority() {
+        let socket: SocketAddressV4 = IPv4Address::LOCALHOST.to_socket(80);
+        let result: Authority = socket.to_authority();
+        let expected: Authority = Authority::new(IPv4Address::LOCALHOST.to_host(), 80);
         assert_eq!(result, expected);
     }
 }
--- src/socket/conversions_v6.rs
+++ src/socket/conversions_v6.rs
@@ -1,4 +1,4 @@
-use crate::{SocketAddress, SocketAddressV6};
+use crate::{Authority, SocketAddress, SocketAddressV6};
 
 impl SocketAddressV6 {
     //! Conversions
@@ -7,17 +7,30 @@ impl SocketAddressV6 {
     pub const fn to_socket(self) -> SocketAddress {
         SocketAddress::new(self.ip().to_ip(), self.port())
     }
+
+    /// Converts the IPv6 socket address to an authority.
+    pub const fn to_authority(self) -> Authority {
+        Authority::new(self.ip().to_host(), self.port())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{IPv6Address, SocketAddress, SocketAddressV6};
+    use crate::{Authority, IPv6Address, SocketAddress, SocketAddressV6};
 
     #[test]
     fn v6_to_socket() {
         let socket: SocketAddressV6 = IPv6Address::LOCALHOST.to_socket(80);
         let result: SocketAddress = socket.to_socket();
         let expected: SocketAddress = SocketAddress::new(IPv6Address::LOCALHOST.to_ip(), 80);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn v6_to_authority() {
+        let socket: SocketAddressV6 = IPv6Address::LOCALHOST.to_socket(80);
+        let result: Authority = socket.to_authority();
+        let expected: Authority = Authority::new(IPv6Address::LOCALHOST.to_host(), 80);
         assert_eq!(result, expected);
     }
 }
