25f3db6c42d793138d583fa999d9046f07184672
Nik Deapen <nik@deapen.com>
2026-09-21 06:39:16 -0700

    [cargo] Updated the version to 0.22.0. (#55)

--- src/display/endpoint.rs
+++ src/display/endpoint.rs
@@ -1,5 +1,9 @@
-use crate::{Endpoint, EndpointRef};
+use crate::{Domain, Endpoint, EndpointRef};
 use std::fmt::{Debug, Display, Formatter};
+use std::io::Write;
+
+/// The length of the longest endpoint text. (the longest name, a colon, & the longest port)
+const MAX_LEN: usize = Domain::MAX_NAME_LEN + ":65535".len();
 
 impl Debug for Endpoint {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -24,7 +28,11 @@ impl<'a> Display for EndpointRef<'a> {
         if f.width().is_none() && f.precision().is_none() {
             write!(f, "{}:{}", self.domain(), self.port())
         } else {
-            f.pad(&format!("{}:{}", self.domain(), self.port()))
+            let mut bytes: [u8; MAX_LEN] = [0; MAX_LEN];
+            let mut cursor: &mut [u8] = &mut bytes;
+            write!(cursor, "{}:{}", self.domain(), self.port()).map_err(|_| std::fmt::Error)?;
+            let len: usize = MAX_LEN - cursor.len();
+            f.pad(unsafe { std::str::from_utf8_unchecked(&bytes[..len]) })
         }
     }
 }
